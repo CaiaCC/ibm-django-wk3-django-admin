a09f6fad3415e264104107df2e4992bf6095e6e6
Caia Chuang <caiachuang@gmail.com>
2023-12-14 12:40:28 -0800

    register models

--- adminsite/admin.py
+++ adminsite/admin.py
@@ -2,3 +2,12 @@ from django.contrib import admin
 from .models import Course, Instructor, Lesson
 
 # Register your models here.
+class CourseAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'name', 'description']
+
+admin.site.register(Course, CourseAdmin)
+
+class InstructorAdmin(admin.ModelAdmin):
+    fields=['user', 'full_time']
+
+admin.site.register(Instructor, InstructorAdmin)
